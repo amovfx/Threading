d0abe523b9e886624263406acaa64a0d61e1bf0f
andrewoseen <amo.vfx@protonmail.com>
2020-10-18 16:01:48 -0400

    Added callable object.

--- Mutex/main.cpp
+++ Mutex/main.cpp
@@ -4,29 +4,41 @@
 
 using namespace std;
 
+class App
+{
+private:
+    const int ITERATIONS = 1E6;
+    int count = 0;
+    mutex mtx;
+
+public:
+    void operator()()
+    {
+        for (int i =0; i < ITERATIONS; i++)
+        {
+            const lock_guard<mutex> guard(mtx);
+            count += 1;
+
+        }
+    }
+
+    int getCount() const {
+        return count;
+    }
+
+};
+
 int main() {
     std::cout << "Hello, Mutex" << std::endl;
 
-    const int ITERATIONS = 1E6;
-    int count = 0;
+    App app;
 
-    mutex mtx;
-
-    auto fn =  [&]() {
-        for (int i =0; i < ITERATIONS; i++)
-        {
-            mtx.lock();
-            count += 1;
-            mtx.unlock();
-        }
-    };
-
-    thread t1(fn);
-    thread t2(fn);
+    thread t1(ref(app));
+    thread t2(ref(app));
 
     t1.join();
     t2.join();
 
-    cout << count;
+    cout << app.getCount();
     return 0;
 }
