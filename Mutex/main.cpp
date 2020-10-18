ebdc27817041b92cf8dcae4a6820594c5eb8cade
andrewoseen <amo.vfx@protonmail.com>
2020-10-18 15:48:50 -0400

    added mutex example.

--- Mutex/main.cpp
+++ Mutex/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <thread>
+
+
+using namespace std;
 
 int main() {
     std::cout << "Hello, Mutex" << std::endl;
+
+    const int ITERATIONS = 1E6;
+    int count = 0;
+
+    mutex mtx;
+
+    auto fn =  [&]() {
+        for (int i =0; i < ITERATIONS; i++)
+        {
+            mtx.lock();
+            count += 1;
+            mtx.unlock();
+        }
+    };
+
+    thread t1(fn);
+    thread t2(fn);
+
+    t1.join();
+    t2.join();
+
+    cout << count;
     return 0;
 }
